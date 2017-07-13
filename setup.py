c329ebf5e35fdb0f59bc67ed50f131ac6502887e
Miguel Daal <mdaal@berkeley.edu>
2017-07-12 18:02:30 -0700

    Added setup.py and the rap/__init__.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rap',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='mdaal',
+      author_email='',
+      license='MIT',
+      packages=['rap'],
+      zip_safe=False)
